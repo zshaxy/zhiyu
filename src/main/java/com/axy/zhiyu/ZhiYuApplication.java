595e0deca86c3fffda30d1529bd7fe13050a76f8
zshaxy <zyzaxy@qq.com>
2020-11-02 16:04:40 +0800

    002

--- src/main/java/com/axy/zhiyu/ZhiYuApplication.java
+++ src/main/java/com/axy/zhiyu/ZhiYuApplication.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class ZhiYuApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZhiYuApplication.class,args);
-		
+        System.out.println("qweqweqeq");
     }
 
 }
